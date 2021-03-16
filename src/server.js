bd3b1dda87a0f67e29f01390e21e8f9c056b502c
df2017 <dferrey22783@gmail.com>
2021-03-15 21:03:16 -0300

    fix run server

--- src/server.js
+++ src/server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.use(express.static('./dist/project-management'));
+app.use(express.static('./dist/projectManagement'));
 app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/project-management/'});
+  res.sendFile('index.html', {root: 'dist/projectManagement/'});
 });
 
 app.listen(process.env.PORT || port);
